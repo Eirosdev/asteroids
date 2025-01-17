406ff4726221cbf17aef711c32d4eb951c5351ce
eirosdev <eivindrostad@gmail.com>
2025-01-17 15:56:17 +0100

    spawn player actor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!usr/bin/python3
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,13 +9,14 @@ def main():
     game_fps = 60
     game_dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) #spawn player in the middle of the screen
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         game_dt = game_clock.tick(game_fps)/1000
 
